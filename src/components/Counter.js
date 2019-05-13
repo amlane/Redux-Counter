1003a82c3bf539aa6bb3967430005202b2b67e82
amlane <twelvexstring@gmail.com>
2019-05-13 15:01:36 -0500

    now counter is displaying NaN

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -13,11 +13,6 @@ class Counter extends Component {
         // increments after waiting for one second
     };
 
-    updateCount = e => {
-        e.preventDefault();
-        this.props.increment()
-    }
-
     render() {
         // Fill in the two button onClick methods
         // Upon clicking these buttons, the count
@@ -25,10 +20,10 @@ class Counter extends Component {
         return (
             <p>
                 Clicked: {this.props.count} times
-                <button onClick={() => {this.updateCount}}>
+                <button onClick={() => this.props.increment()}>
                     +
                 </button>
-                <button onClick={() => {/* Fill me in */ }}>
+                <button onClick={() => this.props.decrement()}>
                     -
                 </button>
                  {/* Uncomment these button tags if you got
@@ -52,7 +47,7 @@ class Counter extends Component {
 // parts it needs from the state object.
 const mapStateToProps = (state) => {
     return {
-        count: state.count
+        count: state
     };
 };
 
